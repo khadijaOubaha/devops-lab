a9c3b843d6669ac45612bf0e98e01c1135cd4511
khadija414 <oubahak28@gmail.com>
2026-04-20 21:24:14 +0200

    last update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version fin waaaaaaaaaaaaaaaaaaaaa - khadija oubaha"
+    return "Version fin  - khadija oubaha"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
