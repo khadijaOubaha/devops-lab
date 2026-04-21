6d824bea3d7a202ef5fcf08b987c274ddf699d74
khadija414 <oubahak28@gmail.com>
2026-04-21 17:58:45 +0200

    indes html

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,26 @@
-FROM python:3.10-slim
+# FROM python:3.10-slim
+
+# WORKDIR /app
+
+# COPY requirements.txt .
+
+# RUN pip install -r requirements.txt
+
+# COPY app.py .
+
+# CMD ["python", "app.py"]
+
+FROM python:3.9-slim
 
 WORKDIR /app
 
+# On copie le fichier des dépendances d'abord (optimisation du cache)
 COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 
-RUN pip install -r requirements.txt
+# On copie tout le reste du projet (templates, static, app.py)
+COPY . .
 
-COPY app.py .
+EXPOSE 5000
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "fin  - khadijaa nassimaaaa"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+# Simule une base de données
+tasks = ["Installer Jenkins", "Configurer Docker", "Réussir le pipeline"]
+
 @app.route("/")
 def home():
-    return "fin  - khadijaa nassimaaaa"
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    task = request.form.get("task")
+    if task:
+        tasks.append(task)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
