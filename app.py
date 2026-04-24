32174b4329e83d50a4aeb39acc688cfbdf394376
khadija414 <oubahak28@gmail.com>
2026-04-24 09:33:28 +0200

    commit 13

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 # @app.route("/")
 # def home():
-#     return "fin  - khadijaa nassimaaaa"
+#     return "fin  - khadijaa "
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=5000)
@@ -14,7 +14,7 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 # Simule une base de données
-tasks = ["Installer Jenkins", "Configurer Docker", "Réussir le pipeline","Visualiser le chngement "]
+tasks = ["Installer Jenkins", "Configurer Docker", "Réussir le pipeline","version final"]
 
 @app.route("/")
 def home():
